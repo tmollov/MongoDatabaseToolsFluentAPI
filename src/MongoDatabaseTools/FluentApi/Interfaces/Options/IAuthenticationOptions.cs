204dcb1cc00e05a3c3e579e06512954ea642b724
Tundzhay Mollov <tundzhaymollov@gmail.com>
2021-11-03 20:03:22 +0200

    Renamed IAuthenticationOptionsBasic to IAuthenticationOptions

--- src/MongoDatabaseTools/FluentApi/Interfaces/Options/IAuthenticationOptions.cs
+++ src/MongoDatabaseTools/FluentApi/Interfaces/Options/IAuthenticationOptions.cs
@@ -3,7 +3,7 @@ namespace FluentApi.Interfaces.Options
     /// <summary>
     /// Interface for authentications options of MongoDB utilities.
     /// </summary>
-    public interface IAuthenticationOptionsBasic
+    public interface IAuthenticationOptions
     {
         /// <summary>
         /// Specifies a username with which to authenticate to a MongoDB database that uses authentication.
@@ -20,10 +20,19 @@ namespace FluentApi.Interfaces.Options
         IMongoUtility Password(string password);
 
         /// <summary>
+        /// Specifies the authentication database where the specified username has been created.
+        /// </summary>
+        /// <param name="databaseName">Name of the database.</param>
+        /// <returns><see cref="IMongoUtility"/>.</returns>
+        IMongoUtility AuthenticationDatabase(string databaseName);
+
+        /// <summary>
         /// Specifies the authentication mechanism the mongo utility instance uses to authenticate to the mongod or mongos.
         /// </summary>
-        /// <param name="mechanism">A mechanism .</param>
+        /// <param name="mechanism">A mechanism.</param>
         /// <returns><see cref="IMongoUtility"/>.</returns>
         IMongoUtility AuthenticationMechanism(string mechanism);
+
+        // TODO: Add AmazonWebServicesSessionToken method.
     }
 }
