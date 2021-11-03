1633716edcb2f95bd66716fa927b055188a9f3ad
Tundzhay Mollov <tundzhaymollov@gmail.com>
2021-11-03 20:07:52 +0200

    Update IMongoUtility.cs
    Now interface inherits Uri method from IUriOptions interface

--- src/MongoDatabaseTools/FluentApi/Interfaces/IMongoUtility.cs
+++ src/MongoDatabaseTools/FluentApi/Interfaces/IMongoUtility.cs
@@ -5,19 +5,12 @@ namespace FluentApi.Interfaces
     /// <summary>
     /// Interface for mongo utility.
     /// </summary>
-    public interface IMongoUtility : IVerbosityOptions, IRunnableUtility
+    public interface IMongoUtility : IVerbosityOptions, IRunnableUtility, IUriOptions
     {
-        /// <summary>
-        /// Specifies a mongodb uri connection string.
-        /// </summary>
-        /// <param name="uri">A mongodb uri connection string.</param>
-        /// <returns><see cref="IMongoUtility"/>.</returns>
-        IMongoUtility Uri(string uri);
-
         /// <summary>
         /// Number of collections to dump in parallel.
         /// </summary>
-        /// <param name="count">Number of parallel collections</param>
+        /// <param name="count">Number of parallel collections.</param>
         /// <returns><see cref="IMongoUtility"/>.</returns>
         IMongoUtility NumberOfParallelCollections(byte count);
     }
